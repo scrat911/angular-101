48e6518feca009a23bf065f561dbd704a71f7826
Luis E <pitufo89@gmail.com>
2021-04-10 19:53:21 +0200

    Add Angular Fire auth guard.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { WallComponent } from './wall/wall.component';
+import { redirectUnauthorizedTo, redirectLoggedInTo, canActivate } from '@angular/fire/auth-guard';
+
+const redirectLoggedIn = () => redirectLoggedInTo(['wall']);
+const redirectforbidden = () => redirectUnauthorizedTo(['login'])
 
 const routes: Routes = [
-  { path: 'wall', component: WallComponent },
   { path: '', redirectTo: '/wall', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent},
-  { path: 'register', component: RegisterComponent},
+  { path: 'login', component: LoginComponent,  ...canActivate(redirectLoggedIn) },
+  { path: 'register', component: RegisterComponent, ...canActivate(redirectLoggedIn) },
+  { path: 'wall', component: WallComponent , ...canActivate(redirectforbidden) },
 ];
 
 @NgModule({
